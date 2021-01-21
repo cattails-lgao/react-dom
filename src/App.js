e234a69f99af584bf3891f87f4b148171d7bfe63
lgao <191553780@qq.com>
2021-01-21 18:59:56 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { message, Button } from 'antd';
 
 export default class App extends Component {
+
+    handler = () => {
+        message.info('This is a normal message');
+    }
+
     render() {
         return (
             <div>
-                App
+                <Button type="primary" onClick={this.handler}>Button</Button>
             </div>
         )
     }
